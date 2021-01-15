103120e36c11ff34c98dd4e4a26c937a7c179720
Alex Thomson <alex.thomson@stats4sd.org>
2021-01-15 13:47:30 +0000

    Adding Inputs = basic

--- app.R
+++ app.R
@@ -1,8 +1,17 @@
 library(shiny)
 library(agridat)
 library(tidyverse)
+library(sortable)
 
 data1<-cox.stripsplit
+data2<-cox.stripsplit
+
+colnames(data2) <- c("rep", "seed", "pesticide", "nitrogen", "length")
+
+datasets <- list(
+    `Study 1` = data1,
+    `Study 2` = data2
+)
 
 classes <- NULL
 
@@ -20,6 +29,8 @@ data1$none<-"1"
 
 data1$x<-data1$soil
 
+labels <- levels(data1$calcium)
+
 ui <- fluidPage(
     
     
@@ -28,18 +39,26 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel(
+            selectInput("dataset", "Select Dataset", choices = names(datasets)),
             selectInput("outcome",
                         "Select Outcome Variable",choices = classes$col[classes$class == "numeric"|classes$class == "integer"]
             ),
             selectInput("x","Select x-axis variable",choices= classes$col[(classes$class == "factor" | classes$class =="character") & classes$col != "none"]),
-            selectInput("colour","Select colour variable",choices=classes$col[classes$class == "factor" | classes$class =="character"]),
+            selectInput("colour","Select colour variable",classes$col[classes$class == "factor" | classes$class =="character"]),
             selectInput("facet","Select facet variable",choices=classes$col[classes$class == "factor" | classes$class =="character"]),
             
             selectInput("order","Arrange x-axis by:",choices=c("Data Order"="data",
                                                                "Increasing y"="increase",
                                                                "Decreasing y"="decrease",
                                                                "Custom"="custom")),
-            htmlOutput("bucketlist")
+            conditionalPanel(
+                condition = "input.order == 'custom'",
+            custom_order <- rank_list(
+                text = "Drag the items in desired x axis order",
+                labels = labels,
+                input_id = "custom_sort"
+            )
+        )
         ),
         
         
@@ -49,8 +68,7 @@ ui <- fluidPage(
     )
 )
 
-
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     output$Plot1 <- renderPlot({
         
@@ -62,12 +80,15 @@ server <- function(input, output) {
         if(input$order=="decrease"){
             data1$x<-reorder(data1$x,-1*data1$yield,mean)
         }
-        
+        if(input$order == "custom"){
+            data1$x <- fct_relevel(data1$x, input$custom_order)
+        }
         
         ggplot(data=data1,aes_string(x="x",colour=input$colour,group=input$colour,y=input$outcome))+
             stat_summary(geom="line")+
             stat_summary(geom="point")+
-            facet_wrap(input$facet)
+            facet_wrap(input$facet)+
+            theme_classic()
     })
 }
 
---
+++ misc stuff to keep track of.R
@@ -0,0 +1,27 @@
+observe({
+  
+  classes1 <- NULL
+  
+  dataX <- datasets[input$dataset]
+  
+  for(i in 1:ncol(dataX)){
+    
+    classes$col[i] <- colnames(dataX)[i]
+    classes$class[i] <- class(dataX[,i]) 
+  }
+  
+  classes <- data.frame(classes)%>%
+    arrange(col)%>%
+    add_row(col = "none", class = "character")
+  
+  dataX$none<-"1"
+  
+  updateSelectInput(session,"outcome",choices=classes$col[classes$class == "numeric"|classes$class == "integer"])
+  
+  updateSelectInput(session,"x",choices=classes$col[(classes$class == "factor" | classes$class =="character") & classes$col != "none"])
+  
+  updateSelectInput(session,"colour",choices=classes$col[classes$class == "factor" | classes$class =="character"])
+  
+  updateSelectInput(session,"facet",choices=classes$col[classes$class == "factor" | classes$class =="character"])
+})
+
